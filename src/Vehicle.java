d6fd552ee9b0fb2bdcf02b8e811bd1b8a96a8347
Jeremiah Gaertner <jamiahx@gmail.com>
2015-10-12 18:33:42 -0400

    Minor changes and refactoring to Vehicle.java and VehicleTester.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,39 +3,49 @@
  * @author emmaperez
  *
  */
+
+import java.time.Year;
+
 public class Vehicle {
 
-	public String make;
-	public String model;
-	public String year;
-	public String type;
-	public String pricePerDay;
-	public String description;
+    private final String make;
+    private final String model;
+    private final Year year;
+    private final String category;
+    protected int rate;
+    protected String description;
 
-	/**
-	 * Vehicle constructor for the Vehicle class
-	 * @param make
-	 * @param model
-	 * @param year
-	 * @param type
-	 * @param pricePerDay
-	 * @param description
-	 */
-	public Vehicle(String make, String model, String year, String type, String pricePerDay, String description)
-	{
-		this.make = make;
-		this.model = model;
-		this.year = year;
-		this.type = type;
-		this.pricePerDay = pricePerDay;
-		this.description = description;
-	}
+    public String getMake(){return make;}
+    public String getModel(){return model;}
+    public Year getYear(){return year;}
+    public String getCategory(){return category;}
+    public int getRate(){return rate;}
+    public String getDescription(){return description;}
+
+    /**
+     * Vehicle constructor for the Vehicle class
+     * @param make Vehicle Manufacturer
+     * @param model 
+     * @param year Model Year of vehicle
+     * @param category Vehicle category
+     * @param rate Price per day rented
+     * @param description Vehicle description
+     */
+    public Vehicle(String make, String model, Year year, String category, int rate, String description)
+    {
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.category = category;
+	this.rate = rate;
+	this.description = description;
+    }
 	
-	/**
-	 * To convert the items in the array list to strings. 
-	 */
-	public String toString()
-	{
-		return make + " " + model + " " + year + " " + type + " " + pricePerDay + " " + description;
-	}
+    /**
+     * To convert the items in the array list to strings. 
+     */
+    public String toString()
+    {
+	return (make + "," + model + "," + year.toString() + "," + category + "," + rate + "," + description);
+    }
 }
--- src/VehicleTester.java
+++ src/VehicleTester.java
@@ -1,39 +1,33 @@
 
 import java.util.ArrayList;
 import java.io.File;
-
 import java.io.IOException;
 import java.util.Scanner;
+import java.time.Year;
 
 
 public class VehicleTester {
 
-	public static void main (String [] args) throws IOException{
+    public static void main (String [] args) throws IOException{
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter your file path. Do not leave a single character out. For example: /Users/emmaperez/Desktop/data(1).txt");
-		String fileName = scanner.nextLine();
-		System.out.println("You selected this file: " + fileName);
-		
-		File file = new File(fileName);
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Please enter your file path. Do not leave a single character out. For example: /Users/emmaperez/Desktop/data(1).txt");
+	File file = new File(scanner.nextLine());
+	System.out.println("You selected this file: " + file.getName());
 
-		Scanner s = new Scanner(file).useDelimiter(", ");
+
+	Scanner s = new Scanner(file).useDelimiter(",");
 	
-		ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+	ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
 		
-		while(s.hasNext())
-		{
-			ArrayList<String> info = new ArrayList<String>();
-			for (int i = 0; i < 6; i++)
-			{
-				info.add(s.next());
-			}
-			vehicles.add(new Vehicle(info.get(0), info.get(1), info.get(2), info.get(3), info.get(4), info.get(5)));
-		}
-		for (int i = 0; i < vehicles.size(); i++)
-		{
-			System.out.println(vehicles.get(i).toString());
-		}
+	while(s.hasNext())
+	    {
+		vehicles.add(new Vehicle(s.next(), s.next(), Year.parse(s.next()), s.next(), Integer.valueOf(s.next()), s.next()));
+	    }
+	for (int i = 0; i < vehicles.size(); i++)
+	    {
+		System.out.println(vehicles.get(i).toString());
+	    }
 
-		}
+    }
 }
