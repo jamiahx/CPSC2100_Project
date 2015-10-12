a6908a4948c56892aa28d02a52b4617ed64cc6c7
Jeremiah Gaertner <jamiahx@gmail.com>
2015-10-12 19:43:43 -0400

    Revert "removed unnecessary imports"
    This reverts commit 00c08640a87a3df0fbb3c8bc5c339985a920a86b.

--- src/main/java/edu/utc/2015cpsc2100/ejkk/People.java
+++ src/main/java/edu/utc/2015cpsc2100/ejkk/People.java
@@ -1,13 +1,33 @@
+/**
+ * Copyright 2015 Emma Perez, jamiahx
+ * jamiahx@gmail.com
+ * 
+ * This file is a part of CPSC2100_ORS.
+ *
+ * CPSC2100_ORS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * CPSC2100_ORS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with CPSC2100_ORS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.security.Principle;
+import javax.security.auth.Subject;
 import java.util.UUID;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.naming.NameAlreadyBoundException;
 import Settings;
 
 
 /// How to instantiate a Customer
-// import javax.security.auth.Subject;
-// import java.util.Vector;
 // new Subject(false, new Vector<Principle> {new User(username, Name(firstName, lastName)), new Customer()}, {}, new Vector<PassHash> {PassHash(passHash)});
 
 
