e375da45d654cfe42033130ea2c072e02d45c5d1
Jeremiah Gaertner <jamiahx@gmail.com>
2015-10-20 14:08:34 -0400

    updated license header for some files

--- src/main/java/edu/utc/2015cpsc2100/ejkk/UUID_Generator.java
+++ src/main/java/edu/utc/2015cpsc2100/ejkk/UUID_Generator.java
@@ -1,6 +1,8 @@
 /**
- * Copyright 2015 jamiahx
+ * Copyright 2015 Emma Perez, jamiahx, Kate Siprelle, Kaleb Sanchez
  * jamiahx@gmail.com
+ * kalebsanchez23@yahoo.com
+ * ksiprelle@gmail.com
  * 
  * This file is a part of CPSC2100_ORS.
  *
@@ -17,6 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with CPSC2100_ORS.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 import java.security.MessageDigest;
 import java.util.Arrays;
