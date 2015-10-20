6652c55cda5dc0c5b6535c1369340f5aac2ef7ca
Kate Siprelle <ksiprelle@gmail.com>
2015-10-20 14:02:57 -0400

    Added searchMultiple method

--- src/main/java/edu/utc/2015cpsc2100/ejkk/Searcher.java
+++ src/main/java/edu/utc/2015cpsc2100/ejkk/Searcher.java
@@ -2,7 +2,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-
 package edu.utc.2015cpsc2100.ejkk;
 
 public class Searcher
@@ -11,19 +10,39 @@ public class Searcher
 	
     public Searcher(ArrayList<Object> listOfObjects)
     {
-	searchableList = listOfObjects;
+    	searchableList = listOfObjects;
     }
 	
     public ArrayList<Object> search(SearchParameter sp)
     {
-	ArrayList<Object> resultObjects = new ArrayList<Object>();
-	for (int i = 0; i < searchableList.size(); i++)
+    	ArrayList<Object> resultObjects = new ArrayList<Object>();
+    	for (int i = 0; i < searchableList.size(); i++)
 	    {
-		if (sp.method.invoke(searchableList.get(i)).equals(sp.result))
+    		if (sp.method.invoke(searchableList.get(i)).equals(sp.result))
 		    {
-			resultObjects.add(searchableList.get(i));
+    			resultObjects.add(searchableList.get(i));
 		    }
 	    }
-	return resultObjects;
+    	return resultObjects;
+    }
+    
+    public ArrayList<Object> searchMultiple(ArrayList<SearchParameter> spList)
+    {
+    	ArrayList<Object> resultObjects = new ArrayList<Object>();
+    	for (int i = 0; i < spList.size(); i++)
+    	{
+    		if (i == 0)
+    		{
+    			resultObjects.add(search(spList.get(i)));
+    		}
+    		else
+    		{
+    			ArrayList<Object> holder = new ArrayList<Object>();
+        		holder.add(resultObjects);
+        		Searcher temp = new Searcher(holder);
+        		resultObjects.clear();
+        		resultObjects.add(Searcher.search(spList.get(i)));
+    		}
+    	}
     }
 }
