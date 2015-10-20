1631f8902889459d88ab1bf4dd8620e139c794bb
Jeremiah Gaertner <jamiahx@gmail.com>
2015-10-20 14:05:10 -0400

    added license header to rmaining added files

--- src/main/java/edu/utc/2015cpsc2100/ejkk/Searcher.java
+++ src/main/java/edu/utc/2015cpsc2100/ejkk/Searcher.java
@@ -1,6 +1,30 @@
+/**
+ * Copyright 2015 Emma Perez, jamiahx, Kate Siprelle, Kaleb Sanchez
+ * jamiahx@gmail.com
+ * kalebsanchez23@yahoo.com
+ * ksiprelle@gmail.com
+ * 
+ * This file is a part of CPSC2100_ORS.
+ *
+ * CPSC2100_ORS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * CPSC2100_ORS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with CPSC2100_ORS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+
 
 package edu.utc.2015cpsc2100.ejkk;
 
@@ -30,19 +54,19 @@ public class Searcher
     {
     	ArrayList<Object> resultObjects = new ArrayList<Object>();
     	for (int i = 0; i < spList.size(); i++)
-    	{
+	    {
     		if (i == 0)
-    		{
+		    {
     			resultObjects.add(search(spList.get(i)));
-    		}
+		    }
     		else
-    		{
+		    {
     			ArrayList<Object> holder = new ArrayList<Object>();
         		holder.add(resultObjects);
         		Searcher temp = new Searcher(holder);
         		resultObjects.clear();
         		resultObjects.add(Searcher.search(spList.get(i)));
-    		}
-    	}
+		    }
+	    }
     }
 }
