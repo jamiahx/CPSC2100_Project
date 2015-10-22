4ddca497cc2733b3b85fad208e6bca6dcadeb8b6
Jeremiah Gaertner <jamiahx@gmail.com>
2015-10-22 17:45:46 -0400

    added PrivInfo&co for protecting private information

---
+++ src/main/java/edu/utc/2015cpsc2100/ejkk/PrivInfo.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2015 Emma Perez, jamiahx, Kate Siprelle, Kaleb Sanchez
+ * jamiahx@gmail.com
+ * kalebsanchez23@yahoo.com
+ * ksiprelle@gmail.com
+ * 
+ * This file is a part of CPSC2100_ORS.
+ *
+ * CPSC2100_ORS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * CPSC2100_ORS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with CPSC2100_ORS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package edu.utc._2015cpsc2100.ejkk;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.security.auth.Destroyable;
+
+
+public abstract class PrivInfo implements Destroyable
+{
+    private static ConcurrentHashMap<UUID, GuardedObject> PrivInfoDB;
+
+    public final PrivInfo getPrivInfo(UUID owner)
+    {
+	return PrivInfoDB.get(UUID_Generator.gen(owner, this.getClass.toString()));
+    }
+
+    protected PrivInfo(UUID owner)
+    {
+	PrivInfoDB.put(UUID_Generator.gen(owner, this.getClass.toString()), this);
+    }
+}
