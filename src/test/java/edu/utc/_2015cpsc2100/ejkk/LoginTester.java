626a0f5a0eed112cd371a0b34eeb3d4fa286e9de
Jeremiah Gaertner <jamiahx@gmail.com>
2015-10-27 00:19:56 -0400

    added LoginTester

---
+++ src/test/java/edu/utc/_2015cpsc2100/ejkk/LoginTester.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2015 Emma Perez, jamiahx, Kate Siprelle, Kaleb Sanchez
+ * jamiahx@gmail.com
+ * kalebsanchez23@yahoo.com
+ * ksiprelle@gmail.com
+ * 
+ * This file is a part of CPSC2100_ORS.
+ *
+ * CPSC2100_ORS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * CPSC2100_ORS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CPSC2100_ORS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package edu.utc._2015cpsc2100.ejkk;
+
+
+public final class LoginTester
+{
+    public static final void main(String[] args)
+    {
+	Login.main();
+    }
+}
