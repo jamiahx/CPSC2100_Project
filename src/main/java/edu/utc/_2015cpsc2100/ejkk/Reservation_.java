19e4140b9bdbb3110b37847da50972d6edf184eb
Jeremiah Gaertner <jamiahx@gmail.com>
2015-11-13 01:11:22 -0500

    populated configuration and metamodel with entity classes

---
+++ src/main/java/edu/utc/_2015cpsc2100/ejkk/Reservation_.java
@@ -0,0 +1,15 @@
+package edu.utc._2015cpsc2100.ejkk;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-11-13T01:06:59.685-0500")
+@StaticMetamodel(Reservation.class)
+public class Reservation_ {
+	public static volatile SingularAttribute<Reservation, Vehicle> vehicle;
+	public static volatile SingularAttribute<Reservation, Integer> reservationDates;
+	public static volatile SingularAttribute<Reservation, Double> price;
+	public static volatile SingularAttribute<Reservation, String> resID;
+	public static volatile SingularAttribute<Reservation, Boolean> canceled;
+}
