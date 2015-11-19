287ea31c9758ba386aeb21388df494f99d3e39be
Jeremiah Gaertner <jamiahx@gmail.com>
2015-11-19 12:34:38 -0500

    added VehicleGUI EJB

---
+++ src/main/java/edu/utc/_2015cpsc2100/ejkk/VehicleGUI.java
@@ -0,0 +1,15 @@
+package edu.utc._2015cpsc2100.ejkk;
+
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateless;
+
+
+/**
+ * Session Bean implementation class VehicleGUI
+ */
+@Stateless
+@Asynchronous
+public class VehicleGUI implements VehicleViewRemote, VehicleViewLocal
+{
+	
+}
